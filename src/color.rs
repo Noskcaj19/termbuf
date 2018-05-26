bdf01190304092e273fc7399c2399c505539c2ca
Noskcaj <kcajdev@gmail.com>
2018-05-26 19:17:30 -0400

    Use macro to impl termion::Color for Color

--- src/color.rs
+++ src/color.rs
@@ -1,5 +1,27 @@
 use termion::color;
 
+macro_rules! impl_color {
+    ($(($pattern:pat => $result:expr),)* $($color:ident),*) => {
+        impl color::Color for Color {
+            fn write_fg(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+                use self::Color::*;
+                match self {
+                    $($pattern => $result.write_fg(f),)*
+                    $($color => color::$color.write_fg(f),)*
+                }
+            }
+
+            fn write_bg(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+                use self::Color::*;
+                match self {
+                    $($pattern => $result.write_bg(f),)*
+                    $($color => color::$color.write_bg(f),)*
+                }
+            }
+        }
+    };
+}
+
 /// Represents a forground or background color for a cell
 ///
 /// See the [termion docs](::termion::color) for details
@@ -25,53 +47,23 @@ pub enum Color {
     LightYellow,
 }
 
-use std::fmt;
-impl color::Color for Color {
-    fn write_fg(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use self::Color::*;
-        match self {
-            AnsiValue(v) => color::AnsiValue(*v).write_fg(f),
-            Rgb(r, g, b) => color::Rgb(*r, *g, *b).write_fg(f),
-            Black => color::Black.write_fg(f),
-            Blue => color::Blue.write_fg(f),
-            Cyan => color::Cyan.write_fg(f),
-            Green => color::Green.write_fg(f),
-            Magenta => color::Magenta.write_fg(f),
-            Red => color::Red.write_fg(f),
-            White => color::White.write_fg(f),
-            Yellow => color::Yellow.write_fg(f),
-            LightBlack => color::LightBlack.write_fg(f),
-            LightBlue => color::LightBlue.write_fg(f),
-            LightCyan => color::LightCyan.write_fg(f),
-            LightGreen => color::LightGreen.write_fg(f),
-            LightMagenta => color::LightMagenta.write_fg(f),
-            LightRed => color::LightRed.write_fg(f),
-            LightWhite => color::LightWhite.write_fg(f),
-            LightYellow => color::LightYellow.write_fg(f),
-        }
-    }
-
-    fn write_bg(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use self::Color::*;
-        match self {
-            AnsiValue(v) => color::AnsiValue(*v).write_bg(f),
-            Rgb(r, g, b) => color::Rgb(*r, *g, *b).write_bg(f),
-            Black => color::Black.write_bg(f),
-            Blue => color::Blue.write_bg(f),
-            Cyan => color::Cyan.write_bg(f),
-            Green => color::Green.write_bg(f),
-            Magenta => color::Magenta.write_bg(f),
-            Red => color::Red.write_bg(f),
-            White => color::White.write_bg(f),
-            Yellow => color::Yellow.write_bg(f),
-            LightBlack => color::LightBlack.write_bg(f),
-            LightBlue => color::LightBlue.write_bg(f),
-            LightCyan => color::LightCyan.write_bg(f),
-            LightGreen => color::LightGreen.write_bg(f),
-            LightMagenta => color::LightMagenta.write_bg(f),
-            LightRed => color::LightRed.write_bg(f),
-            LightWhite => color::LightWhite.write_bg(f),
-            LightYellow => color::LightYellow.write_bg(f),
-        }
-    }
+impl_color! {
+    (AnsiValue(v) => color::AnsiValue(*v)),
+    (Rgb(r, g, b) => color::Rgb(*r, *g, *b)),
+    Black,
+    Blue,
+    Cyan,
+    Green,
+    Magenta,
+    Red,
+    White,
+    Yellow,
+    LightBlack,
+    LightBlue,
+    LightCyan,
+    LightGreen,
+    LightMagenta,
+    LightRed,
+    LightWhite,
+    LightYellow
 }
